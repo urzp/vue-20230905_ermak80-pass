5f24f6542a3c82b4f20b1e3bd6dec78d05151d40
urzp <ermak80_pass@mail.ru>
2023-09-11 13:01:15 +0300

    task 2 implementation

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,50 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const App = defineComponent({
+    name: 'App',
+    data(){
+        return{
+            inputs: {
+                A_operand:'',
+                B_operand:'',
+                operator: '',
+            },
+            result: '',
+        }
+    },
+
+    methods: {
+        ifDef(operand){
+            return  operand === '' ? true : false;
+        },
+
+        calculate( a = this.inputs.A_operand, b = this.inputs.B_operand, operator = this.inputs.operator, ifDef = this.ifDef){
+            if(ifDef(a)||ifDef(b)||!operator) return false
+            if(b == 0 && operator == "divide") return this.result = "Ошибка: деление на 0"
+            switch(operator){
+                case "sum": this.result = a + b; break;
+                case "subtract": this.result = a - b; break;
+                case "multiply": this.result = a * b; break;
+                case "divide": this.result = a / b; break;
+            }
+            return this.result
+        },
+    },
+
+    watch:{
+        inputs:{
+            deep:true,
+            handler(){
+                this.calculate();
+            }
+        },
+    }
+})
+
+
+const app = createApp(App);
+const vm = app.mount('#app');
+
+window.vm = vm; //use debug
