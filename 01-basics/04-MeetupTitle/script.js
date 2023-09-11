b3a5a6015069ee896e63c81ade5e40fa40e92f2c
urzp <ermak80_pass@mail.ru>
2023-09-11 19:47:35 +0300

    task 4 implement

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,29 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+
+const App = defineComponent({
+  name: 'App',
+
+  data(){
+    return{
+      currentMeetupId: '1',
+      currentMeetup: '',
+    }
+  },
+
+  watch:{
+    currentMeetupId:{
+      immediate: true,
+      async handler(){
+        this.currentMeetup = await fetchMeetupById(this.currentMeetupId);
+      }
+    }
+  },
+
+})
+
+const app = createApp(App);
+const vm = app.mount('#app');
+
+window.vm = vm; //use debug
