ecaa64c1389dbb87272a2f3f0a3d41a4ab08ca85
urzp <ermak80_pass@mail.ru>
2023-09-13 12:28:03 +0300

    complite 02-components/01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,6 +4,15 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр и порождать событие
+  props: {
+    count:{
+      type: Number,
+      required: true,
+      default: 0,
+    }
+  },
 
-  template: `<button type="button">1</button>`,
+  emits:['update:count'],
+
+  template: `<button type="button" @click="$emit('update:count', ++count)">{{ count }}</button>`,
 });
