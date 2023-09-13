faa14031b9b6e4445a849e86044c4f4c3ab46293
urzp <ermak80_pass@mail.ru>
2023-09-13 15:47:57 +0300

    complite task_02_04

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -3,19 +3,43 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props:{
+    organizer:{
+      type: String,
+      required: true,
+    },
+    place:{
+      type: String,
+      required: true,     
+    },
+    date:{
+      type: Number,
+      required: true,
+    }
+  },
+
+  computed:{
+    date_text(){
+      return new Date(this.date).toLocaleDateString('en-En', { month: 'short', day: 'numeric' , year: 'numeric' })
+    },
+    date_attr(){
+      return new Date(this.date).toLocaleDateString('fr-CA', { year: 'numeric', month: '2-digit', day: '2-digit'  })
+    }
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="date_attr">{{ date_text }}</time>
       </li>
     </ul>`,
 });
