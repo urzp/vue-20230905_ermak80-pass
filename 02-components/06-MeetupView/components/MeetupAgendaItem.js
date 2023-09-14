8444580f3913cde64865020347eff4ed37e7239b
urzp <ermak80_pass@mail.ru>
2023-09-14 14:54:20 +0300

    complite task_02_6

---
+++ 02-components/06-MeetupView/components/MeetupAgendaItem.js
@@ -0,0 +1,42 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from '../meetupService.js';
+
+export default defineComponent({
+  name: 'MeetupAgendaItem',
+
+  agendaItemIcons,
+  agendaItemDefaultTitles,
+
+  props:{
+    agendaItem:{
+      type: Object,
+      require: true,
+    },
+  },
+
+  methods:{
+    getKeyAgendaIcon(){
+      return this.$options.agendaItemIcons[this.agendaItem.type]
+    },
+    getTitle(){
+      return this.agendaItem.title ? this.agendaItem.title : this.$options.agendaItemDefaultTitles[this.agendaItem.type]
+    }
+  },
+
+  template: `
+    <div class="agenda-item">
+      <div class="agenda-item__col">
+        <img :src=" \`/assets/icons/icon-\${getKeyAgendaIcon()}.svg\` "  class="icon" :alt="getKeyAgendaIcon()" />
+      </div>
+      <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
+      <div class="agenda-item__col">
+        <h3 class="agenda-item__title">{{ getTitle() }}</h3>
+        <p v-if="agendaItem.type === 'talk'" class="agenda-item__talk">
+          <span>{{ agendaItem.speaker }}</span>
+          <span class="agenda-item__dot"></span>
+          <span class="agenda-item__lang">{{ agendaItem.language }}</span>
+        </p>
+        <p v-if="agendaItem.description">{{ agendaItem.description }}</p>
+      </div>
+    </div>`,
+});
--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,6 +1,11 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiAlert from './UiAlert.js';
 import UiContainer from './UiContainer.js';
+import MeetupDescription from './MeetupDescription.js'
+import MeetupCover from './MeetupCover.js'
+import MeetupInfo from './MeetupInfo.js'
+import MeetupAgenda from './MeetupAgenda.js'
+
 
 export default defineComponent({
   name: 'MeetupView',
@@ -8,6 +13,10 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+    MeetupAgenda,
   },
 
   props: {
@@ -20,19 +29,21 @@ export default defineComponent({
   template: `
     <div>
       <!-- meetup cover-->
-
+      <MeetupCover :title="meetup.title" :image="meetup.image"/>
       <UiContainer>
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
             <!-- meetup description -->
-
+            <MeetupDescription :description="meetup.description"/>
             <h3>Программа</h3>
             <!-- meetup agenda -->
-            <UiAlert>Программа пока пуста...</UiAlert>
+            <MeetupAgenda v-if="!!meetup.agenda && !!meetup.agenda.length" :agenda="meetup.agenda" />
+            <UiAlert v-else>Программа пока пуста...</UiAlert>
           </div>
           <div class="meetup__aside">
             <!-- meetup info -->
+            <MeetupInfo :organizer="meetup.organizer" :place="meetup.place" :date="meetup.date" />
           </div>
         </div>
       </UiContainer>
