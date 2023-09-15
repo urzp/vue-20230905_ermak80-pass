d652d0c4420d646e33bd4e63695e7c09896b882c
urzp <ermak80_pass@mail.ru>
2023-09-15 08:44:59 +0300

    change files MeetupAgenda.js MeetupAgendaitem.js from task_02_5 to _6 and _7

--- 02-components/06-MeetupView/components/MeetupAgendaItem.js
+++ 02-components/06-MeetupView/components/MeetupAgendaItem.js
@@ -14,7 +14,7 @@ export default defineComponent({
     },
   },
 
-  methods:{
+  computed:{
     getKeyAgendaIcon(){
       return this.$options.agendaItemIcons[this.agendaItem.type]
     },
@@ -26,11 +26,11 @@ export default defineComponent({
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img :src=" \`/assets/icons/icon-\${getKeyAgendaIcon()}.svg\` "  class="icon" :alt="getKeyAgendaIcon()" />
+        <img :src=" \`/assets/icons/icon-\${getKeyAgendaIcon}.svg\` "  class="icon" :alt="getKeyAgendaIcon" />
       </div>
       <div class="agenda-item__col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">{{ getTitle() }}</h3>
+        <h3 class="agenda-item__title">{{ getTitle }}</h3>
         <p v-if="agendaItem.type === 'talk'" class="agenda-item__talk">
           <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
