584703d80867a34937d9fdbc902a8dee7c276a45
urzp <ermak80_pass@mail.ru>
2023-09-18 15:31:28 +0300

    complite task_03_2

--- 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
+++ 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
@@ -17,30 +17,30 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* _transitions.css */
 
 .fade-list {
   position: relative;
 }
 
-.fade-list > * {
+.fade-list :deep(  > * ) {
   opacity: 1;
   transition: opacity 0.3s ease-out;
 }
 
-.fade-list .fade-list-leave-active {
+.fade-list :deep( .fade-list-leave-active ) {
   position: absolute !important;
   left: 0;
   right: 0;
 }
 
-.fade-list .fade-list-enter-from,
-.fade-list .fade-list-leave-to {
+.fade-list  :deep( .fade-list-enter-from ),
+.fade-list :deep( .fade-list-leave-to ){
   opacity: 0;
 }
 
-.fade-list .fade-list-move {
+.fade-list :deep( .fade-list-move ) {
   transition: transform 0.3s;
 }
 </style>
