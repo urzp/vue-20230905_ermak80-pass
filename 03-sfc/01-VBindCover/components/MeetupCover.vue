c64526374637405d93a5f65a11394ad99ffe0041
urzp <ermak80_pass@mail.ru>
2023-09-18 09:49:53 +0300

    complite task_03_1

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+
+  props:{
+    title:{
+      type: String,
+    },
+    image:{
+      type: String,
+    },
+  },
 };
 </script>
 
@@ -14,9 +23,9 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), */
+  /* url('https://course-vue.javascript.ru/api/images/2'); */
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind('image ? "url("+image+")" : "var(--default-cover)"');
   display: flex;
   flex-direction: column;
   align-items: center;
