8bf2e536b7515fc8bdf68472f57c4fd8c7fdeb56
urzp <ermak80_pass@mail.ru>
2023-10-04 09:50:13 +0300

    complit task_05_2

--- 05-vue-router/02-NotFound/router/index.js
+++ 05-vue-router/02-NotFound/router/index.js
@@ -12,5 +12,7 @@ export const router = createRouter({
       path: '/page-b',
       component: () => import('../views/PageB'),
     },
+    { path: '/page-:pathMatch(.*)*',  component: () => import('../views/PageNotFound.vue')},
+    { path: '/:pathMatch(.*)*',  component: () => import('../views/PageNotFound.vue')},
   ],
 });
