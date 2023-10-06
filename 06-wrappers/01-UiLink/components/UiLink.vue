7b4fa89ecf38a0b850d836bef770531916ca9557
urzp <ermak80_pass@mail.ru>
2023-10-06 13:10:09 +0300

    complite task_06_1

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,10 +1,16 @@
 <template>
-  <a class="link">Link!</a>
+  <component :is="tag" class="link"><slot/></component>
 </template>
 
 <script>
 export default {
   name: 'UiLink',
+  props:{
+    tag:{
+      type:[String, Object, Function],
+      default: 'router-link',
+    }
+  }
 };
 </script>
 
