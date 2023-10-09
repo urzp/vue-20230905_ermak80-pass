c47620f96f6baf5370cbf0bb541aedf62d6fe2e4
urzp <ermak80_pass@mail.ru>
2023-10-09 22:18:33 +0300

    pause

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,5 @@
 <template>
-  <UiInput />
+  <UiInput v-model="modelProxy"/>
 </template>
 
 <script>
@@ -7,7 +7,28 @@ import UiInput from './UiInput.vue';
 
 export default {
   name: 'UiInputDate',
-
   components: { UiInput },
+  props:{
+    modelValue: Number,
+    type:{
+      type: String,
+      default:'date'
+    },
+    step: String,
+  },
+  emits: ['update:modelValue'],
+  computed:{
+    modelProxy:{
+      get(){
+        let date = this.modelValue
+        if(this.type == 'date') return new Date(date).toLocaleDateString('en-GB')
+        if(this.type == 'time') return new Date(date).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })
+        return this.modelValue;
+      },
+      set(value){
+        this.$emit('update:modelValue', value);
+      }
+    }
+  },
 };
 </script>
