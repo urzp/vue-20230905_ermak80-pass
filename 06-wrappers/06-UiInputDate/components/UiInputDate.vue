f1af6811f8b7ed3bdb13ef776cae32996d71d881
urzp <ermak80_pass@mail.ru>
2023-10-10 16:23:39 +0300

    pause

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,11 +1,12 @@
 <template>
-  <UiInput v-model="modelProxy"/>
+  <UiInput ref="input" v-bind="$attrs" :type="type" :step="step" :modelValue="modelProxy" />
 </template>
 
 <script>
 import UiInput from './UiInput.vue';
 
 export default {
+  inheritAttrs: false,
   name: 'UiInputDate',
   components: { UiInput },
   props:{
@@ -21,12 +22,26 @@ export default {
     modelProxy:{
       get(){
         let date = this.modelValue
-        if(this.type == 'date') return new Date(date).toLocaleDateString('en-GB')
-        if(this.type == 'time') return new Date(date).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })
-        return this.modelValue;
+        if(this.type == 'date'){ date = new Date(date).toISOString().split('T')[0];} //date = "2020-02-01"
+        if(this.type == 'time'){ date = new Date(date).toISOString().split('T')[1].slice(0, 5) } //date = "10:00" 
+        if(this.type == 'datetime-local'){  // date = "2020-02-01 10:00"
+           date = new Date(date).toISOString().split('T')[0] + "T" +  new Date(date).toLocaleTimeString('en-US', { hour: 'numeric', hour12: false, minute: 'numeric' })
+         }
+        //console.log('type =', this.type, 'in=' ,date)
+        return date
       },
       set(value){
-        this.$emit('update:modelValue', value);
+        if (!value) return null
+         //console.log('type =', this.type,'out =', value )
+        if(this.type == 'date') value = new Date(value).getTime()
+        if(this.type == 'time') value = Date.UTC(1970, 0, 1, value.split(':')[0], value.split(':')[1], 0)
+        if(this.type == 'datetime-local') {
+          let date = value.split("T")[0]
+          let time = value.split("T")[1]
+          value = new Date(date.split("-")[0], date.split("-")[1], date.split("-")[2], time.split(':')[0], time.split(':')[1], 0)
+        }
+        //console.log('out_mode = ', value)
+        this.$emit('update:modelValue',value);
       }
     }
   },
