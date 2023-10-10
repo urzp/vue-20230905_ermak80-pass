18009129176b6bee3cccc916fb7c19f6f796d24d
urzp <ermak80_pass@mail.ru>
2023-10-11 00:23:41 +0300

    complite task_06_6

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,10 @@
-<template>
-  <UiInput ref="input" v-bind="$attrs" :type="type" :step="step" :modelValue="modelProxy" />
+eslint-disable vue/valid-v-slot 
+<template >  
+   <UiInput ref="input" v-bind="$attrs" :type="type" :step="step" v-model="modelProxy">
+    <template v-if="$slots['left-icon']" #left-icon>
+      <slot name="left-icon" />
+    </template>
+   </UiInput>
 </template>
 
 <script>
@@ -22,10 +27,11 @@ export default {
     modelProxy:{
       get(){
         let date = this.modelValue
-        if(this.type == 'date'){ date = new Date(date).toISOString().split('T')[0];} //date = "2020-02-01"
-        if(this.type == 'time'){ date = new Date(date).toISOString().split('T')[1].slice(0, 5) } //date = "10:00" 
-        if(this.type == 'datetime-local'){  // date = "2020-02-01 10:00"
-           date = new Date(date).toISOString().split('T')[0] + "T" +  new Date(date).toLocaleTimeString('en-US', { hour: 'numeric', hour12: false, minute: 'numeric' })
+        if(!date) return ''
+        if(this.type == 'date'){ date = new Date(date).toISOString().split('T')[0];} 
+        if(this.type == 'time'){ date = new Date(date).toISOString().split('T')[1].slice(0, 5) } 
+        if(this.type == 'datetime-local'){  
+           date = new Date(date).toISOString().split('T')[0] + "T" + new Date(date).toISOString().split('T')[1].slice(0, 5) //new Date(date).toLocaleTimeString('en-US', { hour: 'numeric', hour12: false, minute: 'numeric' })
          }
         //console.log('type =', this.type, 'in=' ,date)
         return date
@@ -38,9 +44,9 @@ export default {
         if(this.type == 'datetime-local') {
           let date = value.split("T")[0]
           let time = value.split("T")[1]
-          value = new Date(date.split("-")[0], date.split("-")[1], date.split("-")[2], time.split(':')[0], time.split(':')[1], 0)
+          value = Date.UTC(date.split("-")[0], date.split("-")[1]-1, date.split("-")[2], time.split(':')[0], time.split(':')[1], 0)
         }
-        //console.log('out_mode = ', value)
+        //console.log('out_mode = ', new Date(value))
         this.$emit('update:modelValue',value);
       }
     }
