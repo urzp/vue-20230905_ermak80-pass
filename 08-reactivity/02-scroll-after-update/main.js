92f463c641456dab002a23897ce3ec994c21bddb
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-10-12 09:32:04 +0300

    Added task Скроллинг после обновления

---
+++ 08-reactivity/02-scroll-after-update/main.js
@@ -0,0 +1,11 @@
+import '@/assets/styles/taskbook.css';
+import '@/assets/styles/app.css';
+import '@/assets/styles/_container.css';
+import '@/assets/styles/_button.css';
+import '@/assets/styles/_input-group.css';
+import '@/assets/styles/_layout.css';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
